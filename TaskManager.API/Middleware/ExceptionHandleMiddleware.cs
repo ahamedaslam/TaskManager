d7f88947b33e13eddb42a80fe7cf0ba6e6e7c208
Aslam <Alghouse>
2025-06-09 20:24:21 +0530

    Exception Middleware added

---
+++ TaskManager.API/Middleware/ExceptionHandleMiddleware.cs
@@ -0,0 +1,45 @@
+﻿namespace TaskManager.Middleware
+{
+    public class ExceptionHandleMiddleware
+    {
+        private readonly ILogger<ExceptionHandleMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandleMiddleware(RequestDelegate next, ILogger<ExceptionHandleMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                var errorId = Guid.NewGuid().ToString();
+                _logger.LogError(ex, "ErrorId: {ErrorId} | Exception Message: {ExceptionMessage}", errorId, ex.Message);
+
+                if (!context.Response.HasStarted)
+                {
+                    context.Response.Clear();
+                    context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                    context.Response.ContentType = "application/json";
+                    var errorResponse = new
+                    {
+                        Error = "We apologize, but something went wrong on our end. Please try again later or contact support if the issue continues.",
+                        ErrorId = errorId
+                    };
+                    await context.Response.WriteAsJsonAsync(errorResponse);
+                }
+                else
+                {
+                    _logger.LogWarning("The response has already started, the error handler will not be executed.");
+                }
+            }
+        }
+    }
+}
+
